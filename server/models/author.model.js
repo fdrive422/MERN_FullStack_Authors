521b84776e6cabf26225e6df0d12940b8ab48dae
fdrive422 <fafrago@gmail.com>
2022-03-21 11:10:27 -0700

    timestamps added to models.js

--- server/models/author.model.js
+++ server/models/author.model.js
@@ -6,7 +6,7 @@ const AuthorSchema = new mongoose.Schema({
         required: [true, "Author name is required."],
         minlength: [3, "Name length must be at least 3 characters."],
     },
-});
+}, { timestamps: true });
 
 const Author = mongoose.model("Author", AuthorSchema);
 
